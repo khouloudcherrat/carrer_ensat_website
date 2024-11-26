5ac5ad632d46a55e0040e1a0bddd168da288b0cc
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2024-11-26 11:41:23 +0100

    Create the socle of the application

---
+++ EApp.SpringAPI/src/main/java/com/ENSATApp/EApp/EAppApplication.java
@@ -0,0 +1,13 @@
+package com.ENSATApp.EApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAppApplication.class, args);
+	}
+
+}
