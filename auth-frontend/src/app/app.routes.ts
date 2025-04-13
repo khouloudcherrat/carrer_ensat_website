93a45d3015fc625f8c7e21440994f5a6a39d36bf
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2025-04-13 02:02:20 +0200

    feat: add thank you page after successful sign-up

--- auth-frontend/src/app/app.routes.ts
+++ auth-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { UpdatePasswordComponent } from './auth/update-password/update-password.component';
+import { ThankYouComponent } from './auth/thank-you/thank-you.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'update-password', component: UpdatePasswordComponent }
+  { path: 'update-password', component: UpdatePasswordComponent },
+  { path: 'thank-you', component: ThankYouComponent }
 ];
