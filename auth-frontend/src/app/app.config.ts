60140c4fa9496eb19ae3ffc0a6fc6ea9464beeed
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2025-04-13 02:01:35 +0200

    chore: register HTTP interceptor in app config

--- auth-frontend/src/app/app.config.ts
+++ auth-frontend/src/app/app.config.ts
@@ -3,13 +3,19 @@ import { provideRouter } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { TokenInterceptor } from './auth/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(ReactiveFormsModule)
+    importProvidersFrom(ReactiveFormsModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ]
 };
