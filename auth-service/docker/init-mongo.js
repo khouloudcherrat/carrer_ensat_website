f287a165d8916584e78c0ac9774f0673076557f4
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2025-04-17 01:27:17 +0200

    chore: add admin login credentials to loginInfo collection

--- auth-service/docker/init-mongo.js
+++ auth-service/docker/init-mongo.js
@@ -8,10 +8,26 @@ db.createCollection("alumni");
 db.createCollection("student");
 
 // Insert sample data
-db.loginInfo.insert({
-    email: "admin@example.com",
-    password: "hashedPassword",
-    role: "admin",
-    createdAt: new Date(),
-    updatedAt: new Date()
-});
+db.loginInfo.insertMany([
+    {
+      email: "admin@example.com",
+      password: "$2a$12$AwRfjEJaXyEm7rf1k.Iee.O0.HO617NNRnrFS75jnfW6OaFL4kzt2", // "adminpass"
+      role: "admin",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      email: "student1@example.com",
+      password: "$2a$12$hDkPC1Lhwqoh7sW7N00PsOSGvf/hY1Vheq0tms.oVizHhk2XzAvn6", // "studentpass"
+      role: "student",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      email: "alumni1@example.com",
+      password: "$2a$12$feVgkSM0Sy.HKdu.4hhX2e78vSQYS4/icDnbCNttf8lRMoMkyDPpq", // "alumnipass"
+      role: "alumni",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]);
