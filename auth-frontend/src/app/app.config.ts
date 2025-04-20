5fff510429805475a2c8e395860a6a273be99de2
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2025-04-20 20:35:52 +0200

    Chore: declaring properly the interceptor in the config

--- auth-frontend/src/app/app.config.ts
+++ auth-frontend/src/app/app.config.ts
@@ -3,19 +3,14 @@ import { provideRouter } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TokenInterceptor } from './auth/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
     importProvidersFrom(ReactiveFormsModule),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true
-    }
+    provideHttpClient(withInterceptors([TokenInterceptor])),
   ]
 };
