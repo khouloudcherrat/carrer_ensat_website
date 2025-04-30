ad5fd10202f94a4ba6f36cbfa30cd5ffd0051e20
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2025-04-30 22:55:01 +0200

    feat: add JWT authentication filter to secure backend requests

---
+++ auth-service/src/main/java/com/ENSATApp/EApp/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.ENSATApp.EApp;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+                                    throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null) {
+            String tokenParam = request.getParameter("token");
+            if (tokenParam != null) {
+                authHeader = "Bearer " + tokenParam;
+                System.out.println("✅ Token found in URL parameter");
+            }
+        }
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            if (jwtTokenProvider.validateToken(token)) {
+                String username = jwtTokenProvider.getUsernameFromToken(token);
+
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                String role = jwtTokenProvider.getRoleFromToken(token);
+                System.out.println("Extracted ROLE from token: " + role);
+                if (role != null) {
+                    authorities.add(new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()));
+                }
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
