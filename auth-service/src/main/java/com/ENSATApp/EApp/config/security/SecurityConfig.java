0b9c73bafd0434fa673493902181a44408045bff
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2025-07-20 17:47:59 +0200

    fix: update backend CORS config and frontend API URLs for Cloud

--- auth-service/src/main/java/com/ENSATApp/EApp/config/security/SecurityConfig.java
+++ auth-service/src/main/java/com/ENSATApp/EApp/config/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://frontend-338013605344.europe-west9.run.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
